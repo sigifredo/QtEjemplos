270efd1d8d3ff1a5e147f5d1cdb0896c69d3fd36
Sigifredo Escobar <desarrollofront1ude@udea.edu.co>
2020-01-29 21:55:27 -0500

    fingerpaint example added.

---
+++ touch/fingerpaint/config/project.cmake
@@ -0,0 +1,8 @@
+include_directories(include)
+set(CMAKE_CXX_STANDARD 14)
+
+set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/bin)
+set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/lib)
+set(CMAKE_LIBRARY_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/lib)
+
+add_definitions( -Wall -Wextra -O3 )
